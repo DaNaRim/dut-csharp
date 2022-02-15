d905e3298f1ae1698fe39bb35f587c17c80092aa
DaNaRim <rangar.danarim@gmail.com>
2022-02-15 20:28:00 +0200

    lesson3 homework

---
+++ lesson3/homework/Homework.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp1.lesson3 {
+    public class Homework {
+        public static void Main(string[] args) {
+            Console.WriteLine(MultNums());
+            Console.WriteLine(OperationsWithNums());
+            Console.WriteLine(getColor(Fruits.KIVI));
+
+            Project project = new("BestName", 20, 100, true);
+            Project project2 = new("BestName2", 22, 92, false);
+
+            project.WriteNameAndMark();
+            project.WriteNameAndStatus();
+            project2.WriteNameAndMark();
+            project2.WriteNameAndStatus();
+        }
+
+        public static int MultNums() {
+            return Int32.Parse(Console.ReadLine())
+                   * Int32.Parse(Console.ReadLine())
+                   * Int32.Parse(Console.ReadLine());
+        }
+
+        public static int OperationsWithNums() {
+            int[] nums = new int[3];
+            for (int i = 0; i < nums.Length; i++) {
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            char operand = Console.ReadLine()[0];
+            switch (operand) {
+                case '+': return nums[0] + nums[1] + nums[2];
+                case '*': return nums[0] * nums[1] * nums[2];
+                default: return 0;
+            }
+        }
+
+        public static string getColor(Fruits fruit) {
+            switch (fruit) {
+                case Fruits.PAPAYA: 
+                case Fruits.BANANA: return "yellow";
+                
+                case Fruits.APPLE:
+                case Fruits.PEACH:
+                case Fruits.CHERRY: return "red";
+                
+                case Fruits.KIVI: return "brown";
+                case Fruits.ORANGE: return "orange";
+                default: return "Invalid color";
+            }
+        }
+    }
+
+    public enum Fruits {
+        BANANA,
+        APPLE,
+        KIVI,
+        CHERRY,
+        PEACH,
+        PAPAYA,
+        ORANGE
+    }
+}
---
+++ lesson3/homework/Project.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1.lesson3 {
+    public class Project {
+        private string name;
+        private int daysInWork;
+        private int mark;
+        private bool isFinished;
+
+        public Project(string name, int daysInWork, int mark, bool isFinished) {
+            this.name = name;
+            this.daysInWork = daysInWork;
+            this.mark = mark;
+            this.isFinished = isFinished;
+        }
+
+        public string WriteNameAndMark() {
+            return $"{nameof(name)}: {name}, {nameof(mark)}: {mark}";
+        }
+        
+        public string WriteNameAndStatus() {
+            return $"{nameof(name)}: {name}, {nameof(isFinished)}: {isFinished}";
+        }
+    }
+}
