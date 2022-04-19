cfd1b46a487a3441153585ca6e4ab2972d2bb338
DaNaRim <rangar.danarim@gmail.com>
2022-04-19 21:19:37 +0300

    ADD lesson 7 and comment main method in lesson 6

---
+++ lesson7/Demo.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1.lesson7 {
+    public class Demo {
+        public static void Main(string[] args) {
+            Printer printer = new("asd123", "2031", true, 4);
+            printer.PrintNameAndYearOfManufacturing();
+            printer.PrintNameAndWarrantyPeriod();
+
+            LaserPrinter lp = new("asd123", "2031", true, 4, new[] {"linux, ParrotOS"}, 123);
+            lp.PrintNameAndPrintingSpeed();
+            lp.PrintYearOfManufacturingAndSupportedOs();
+
+            Rabbit rabbit = new(10);
+            rabbit.makeSound();
+            rabbit.move();
+
+            Cat cat = new(9);
+            cat.makeSound();
+            cat.move();
+
+            Goat goat = new(7193257908364501236);
+            goat.makeSound();
+            goat.move();
+        }
+    }
+}
