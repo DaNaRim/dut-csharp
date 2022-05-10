07c4f99ea4eb9c3a0aea6cee25ab3b05d1fd2c74
DaNaRim <rangar.danarim@gmail.com>
2022-05-10 13:19:47 +0300

    lesson 9 comment main method

--- lesson09/Program.cs
+++ lesson09/Program.cs
@@ -5,15 +5,15 @@ namespace ConsoleApp1.lesson09 {
 
     public class Program {
 
-        public static void Main(string[] args) {
-            WelcomeDelegate.sayHelloDel.Invoke();
-
-            MyTextDelegate.printTextTwiceDel.Invoke("abc");
-            Console.WriteLine(MyTextDelegate.getFirstSignDel.Invoke("abc"));
-
-            Console.WriteLine(MyMathDelegate.sumDel.Invoke(1, 2));
-            Console.WriteLine(MyMathDelegate.multiplyDel.Invoke(1, 2));
-        }
+        // public static void Main(string[] args) {
+        //     WelcomeDelegate.sayHelloDel.Invoke();
+        //
+        //     MyTextDelegate.printTextTwiceDel.Invoke("abc");
+        //     Console.WriteLine(MyTextDelegate.getFirstSignDel.Invoke("abc"));
+        //
+        //     Console.WriteLine(MyMathDelegate.sumDel.Invoke(1, 2));
+        //     Console.WriteLine(MyMathDelegate.multiplyDel.Invoke(1, 2));
+        // }
 
     }
 
