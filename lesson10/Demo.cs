d5d821c0463760ab666a6f1491b0241c5184b233
DaNaRim <rangar.danarim@gmail.com>
2022-05-10 13:19:58 +0300

    ADD lesson 10

---
+++ lesson10/Demo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1.lesson10 {
+
+    public class Demo {
+
+        public static void Main() {
+            User user1 = new ("Tom", 2000, "aezakmi", "superPassword");
+            User user2 = new ("Jerry", 2010, "dut@g.com", "123");
+            
+            user1.ageListener += DisplayAge;
+            user2.loginListener += IsUserConfirmed;
+            
+            user1.GetAge();
+            user1.SetYearOfBirth(1000);
+            
+            user2.Login();
+            
+            void DisplayAge(string message, int age) {
+                Console.WriteLine(message + " user age = " + age);
+            }
+
+            void IsUserConfirmed(string message) {
+                Console.WriteLine(message);
+            }
+        }
+        
+    }
+
+}
---
+++ lesson10/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp1.lesson10 {
+
+    public class User {
+
+        private string name;
+        private int yearOfBirth;
+        private string email;
+        private string password;
+        private bool status;
+
+        public  delegate void AgeListener(string message, int age);
+
+        public event AgeListener ageListener;
+
+        public delegate void LoginListener(string message);
+
+        public event LoginListener loginListener;
+
+        public User(string name, int yearOfBirth, string email, string password) {
+            this.name = name;
+            this.yearOfBirth = yearOfBirth;
+            this.email = email;
+            this.password = password;
+            status = false;
+        }
+
+
+        public int GetAge() {
+            int age = DateTime.Now.Year - yearOfBirth;
+
+            if (age < 18) ageListener?.Invoke("The user is a minor", age);
+            if (age > 120) ageListener?.Invoke("User is a vampire", age);
+
+            return age;
+        }
+
+        public void SetYearOfBirth(int yearOfBirth) {
+            ageListener?.Invoke("User changed his age", DateTime.Now.Year - yearOfBirth);
+            this.yearOfBirth = yearOfBirth;
+        }
+
+        public void Login() {
+            if (email == "dut@g.com" && password == "123") {
+                status = true;
+                loginListener?.Invoke("User confirmed");
+            } else if (email == "dut@g.com") {
+                loginListener?.Invoke("Wrong password");
+            } else {
+                loginListener?.Invoke("Wrong username");
+            }
+        }
+
+    }
+
+}
